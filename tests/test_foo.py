f573e63b71b5bedc2b9e0cd5e8b6ab99eda060ed
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-09-13 01:40:18 +0000

    feat: Add tests for foo2 and foo3
    Add tests for the new functions foo2 and foo3 in the `test_foo` module.

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -3,3 +3,11 @@ from test_sah.foo import foo
 
 def test_foo():
     assert foo("foo") == "foo"
+
+
+def test_foo2():
+    assert foo("foo") == "foo"
+
+
+def test_foo3():
+    assert foo("foo") == "foo"
