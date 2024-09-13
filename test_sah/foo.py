cc6782ce1f489181387d9d4910058b0ae381dea5
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-09-13 08:41:27 +0000

    feat: Update dev dependencies in pyproject.toml

--- test_sah/foo.py
+++ test_sah/foo.py
@@ -28,20 +28,5 @@ def foo2(bar: str) -> str:
     return bar
 
 
-def foo3(bar: str) -> str:
-    """Summary line.
-
-    Extended description of function.
-
-    Args:
-        bar: Description of input argument.
-
-    Returns:
-        Description of return value
-    """
-
-    return bar
-
-
 if __name__ == "__main__":  # pragma: no cover
     pass
